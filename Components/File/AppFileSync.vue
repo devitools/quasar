6667925625e9cef193e3e3e6946839dd99f4f814
William Correa <wilcorrea@gmail.com>
2020-04-18 02:29:29 -0300

    [feature] Improve App File Sync

--- Components/File/AppFileSync.vue
+++ Components/File/AppFileSync.vue
@@ -1,45 +1,77 @@
 <template>
     <QFile
-      v-model="model"
+      ref="file"
       v-bind="bind"
+      :value="input"
       @input="updateValue"
-    />
+    >
+        <template v-slot:append>
+            <q-btn
+              round
+              dense
+              flat
+              icon="attach_file"
+              @click="pickFiles"
+            />
+        </template>
+    </QFile>
 </template>
 
 <script>
 import { QFile } from 'quasar'
 
 export default {
-  /**
-   */
-  name: 'AppFileSync',
-  /**
-   */
-  components: { QFile },
-  /**
-   */
-  computed: {
-    bind () {
-      return { ...this.$attrs, ...this.$props }
-    }
-  },
-  /**
-   */
-  data: () => ({
-    model: undefined
-  }),
-  /**
-   */
-  methods: {
     /**
-     * @param {File} $event
      */
-    updateValue ($event) {
-      if ($event === undefined) {
-        return
-      }
-      this.$emit('input', $event)
+    name: 'AppFileSync',
+    /**
+     */
+    components: { QFile },
+    /**
+     */
+    props: {
+        value: {
+            type: [File, FileList, Array, Object, String],
+            default: null
+        }
+    },
+    /**
+     */
+    computed: {
+        bind () {
+            return { ...this.$attrs, ...this.$props }
+        },
+        input () {
+            if (typeof this.value === 'string') {
+                return null
+            }
+            return this.value
+        }
+    },
+    /**
+     */
+    data: () => ({
+        model: null,
+    }),
+    /**
+     */
+    methods: {
+        /**
+         * @param {File} $event
+         */
+        updateValue ($event) {
+            if ($event === undefined) {
+                this.$emit('input', this.model)
+                return
+            }
+            this.model = $event
+            this.$emit('input', $event)
+        },
+        /**
+         */
+        pickFiles () {
+            this.$refs.file.pickFiles()
+        }
     }
-  }
 }
 </script>
