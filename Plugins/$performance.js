0b2329bd6fa9348d8c7330af37bf38692a3e3d5b
William Correa <wilcorrea@gmail.com>
2020-06-28 12:15:31 -0300

    [feature] Use flag to show performance data

--- Plugins/$performance.js
+++ Plugins/$performance.js
@@ -3,17 +3,24 @@
  */
 const performance = {}
 
-/**
- */
-export default {
-  start (reference) {
-    performance[reference] = window.performance.now()
-  },
-  end (reference) {
-    const t1 = window.performance.now()
-    const t0 = performance[reference]
-    if (process.env.NODE_ENV !== 'production' && process.env.VUE_APP_PERFORMANCE === 'true') {
-      console.warn(`[${reference}] ${Math.round(t1 - t0)}ms`)
+const $ = process.env.VUE_APP_PERFORMANCE === 'true'
+  ? {
+    start (reference) {
+      performance[reference] = window.performance.now()
+    },
+    end (reference) {
+      const t1 = window.performance.now()
+      const t0 = performance[reference]
+      if (process.env.NODE_ENV !== 'production') {
+        console.warn(`[${reference}] ${Math.round(t1 - t0)}ms`)
+      }
     }
   }
-}
+  : {
+    start () {},
+    end () {}
+  }
+
+/**
+ */
+export default $
