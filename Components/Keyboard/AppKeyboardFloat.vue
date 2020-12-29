02417a45b2469bd9aef1308de5898e81f4d1b5f4
William Correa <wilcorrea@gmail.com>
2020-12-29 18:33:09 -0300

    [feature] Improve keyboard components

--- Components/Keyboard/AppKeyboardFloat.vue
+++ Components/Keyboard/AppKeyboardFloat.vue
@@ -46,6 +46,13 @@ export default {
   bottom: 0;
   transform: translateY(100vh)
   transition: 0.3s;
+  left: 0;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-items: baseline;
+  z-index: 1000;
 
   &.AppKeyboardFloat--visible {
     transform: translateY(0)
@@ -58,10 +65,10 @@ export default {
   .AppKeyboardFloat__display {
     padding: 5px 10px;
     background: #fff;
-    height: 70px;
+    height: 73px;
     box-shadow: 0 1px 5px rgba(0,0,0,0.2), 0 2px 2px rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12);
     border-radius: 5px 5px 0 0;
-    font-size: 3em;
+    font-size: 2.8em;
     overflow: auto;
     width: 992px;
   }
