00e485f2152ab67f40231971ad6f18c0e186003e
William Correa <wilcorrea@gmail.com>
2021-03-31 10:25:40 -0300

    [feature] Change my account label

--- Lang/pt-br/app.js
+++ Lang/pt-br/app.js
@@ -22,12 +22,8 @@ export default {
       caption: 'Finaliza a sessão atual e redireciona para a tela inicial'
     },
     profile: {
-      label: 'Perfil',
+      label: 'Minha Conta',
       caption: 'Exibe e administra os dados do usuário conectado'
-    },
-    revenue: {
-      label: 'Assinatura',
-      caption: 'Administre suas informações de assinatura'
     }
   },
   http: {
