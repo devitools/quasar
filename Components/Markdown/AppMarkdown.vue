3b39aead238bdc8c4b27dd03619fbb7451e68d7f
William Correa <wilcorrea@gmail.com>
2023-07-24 21:35:28 -0300

    feature: review markdown css rules

--- Components/Markdown/AppMarkdown.vue
+++ Components/Markdown/AppMarkdown.vue
@@ -150,31 +150,32 @@ export default {
   position: relative;
 
   h1 {
-    font-size: 2.2rem;
-  }
-
-  h2 {
-    font-size: 2rem;
-  }
-
-  h3 {
-    font-size: 1.8rem;
-  }
-
-  h4 {
     font-size: 1.6rem;
   }
 
-  h5 {
+  h2 {
     font-size: 1.4rem;
   }
 
+  h3 {
+    font-size: 1.3rem;
+  }
+
+  h4 {
+    font-size: 1.2rem;
+  }
+
+  h5 {
+    font-size: 1.1rem;
+  }
+
   h6 {
-    font-size: 1.25rem;
+    font-size: 1.05rem;
   }
 
   h1, h2, h3, h4, h5, h6 {
-    margin: 0;
+    margin: 5px 0;
+    line-height: 150%;
   }
 
   .q-field__toolbar {
